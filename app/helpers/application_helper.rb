9e29481fae591eb42e9793bfd7d8d0050f77a3b7
Henrique Breim <henrique@breim.com.br>
2014-11-17 21:55:37 -0200

    Novos helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,17 @@
 module ApplicationHelper
+	# Helper do menu_lateral_users
 	def is_active?(link_path)
 		current_page?(link_path) ? "active" : ""
 	end
+
+	# Formata a data conforme sua descricao
+	def dia_mes_ano(date)
+		date.strftime("%d/%m/%Y")
+	end
+
+	# Serve para exibir espacos
+	def texto(s)
+		s.gsub(/\n/, '<br>')
+	end
+
 end
