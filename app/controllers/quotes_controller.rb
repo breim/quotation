912e45e6f0719dc23a559a3522474ba2b600f223
Henrique Breim <henrique@breim.com.br>
2014-11-17 21:58:57 -0200

    Adicionando relacionamentos, pegando somente usuários com acesso a empresas atráves da tabela invitations e funcionaries, que sejam o current_user e que são authorized.

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -18,6 +18,8 @@ class QuotesController < ApplicationController
   end
 
   def painel
+    @cotacoes_enviadas = current_user.quotes.order("created_at desc").page(params[:page]).per(20)
+    @cotacoes_recebidas = Quote.includes(:companies => :funcionaries).where('funcionaries.user_id' => current_user).where('funcionaries.authorized' => 1).order("quotes.created_at desc").page(params[:page_enviada]).per(20)
   end
  
   def select_companies
